01718de4a7563a70642cfefa933ce7e48de4ae4e
Sam Odum <sam.odum1@gmail.com>
2020-01-13 23:50:15 +0100

    Note component to rework

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Welcome() {
-    return (
+    return (// Rework this page
         <div className='App-main-welcome'>
             <h1 className='App-heading-text'>Welcome to FaceApp</h1>
             <p className='App-heading-ptext'>The application that intelligently identifies faces in any images you link to.</p>
