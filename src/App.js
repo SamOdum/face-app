a188e5f68df7c526fb137e669fbceda49189772f
Sam Odum <sam.odum1@gmail.com>
2020-01-15 23:58:48 +0100

    Configure login/signup form

--- src/App.js
+++ src/App.js
@@ -1,71 +1,72 @@
-import React, { createContext, useEffect, useReducer } from "react";
+import React, { createContext, useEffect, useReducer } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import "./App.scss";
-import Login from "./components/Login";
-import Home from "./components/Home";
-import Header from "./components/Header";
+import './App.scss';
+import Login from './components/Login';
+import Home from './components/Home';
+import Header from './components/Header';
 
 export const AuthContext = createContext();
 
 const initialState = {
-  isAuthenticated: false,
-  user: null,
-  token: null,
+	isAuthenticated: false,
+	user: null,
+	token: null,
 };
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case "LOGIN":
-      localStorage.setItem("user", JSON.stringify(action.payload.user));
-      localStorage.setItem("token", JSON.stringify(action.payload.token));
-      return {
-        ...state,
-        isAuthenticated: true,
-        user: action.payload.user,
-        token: action.payload.token
-      };
-    case "LOGOUT":
-      localStorage.clear();
-      return {
-        ...state,
-        isAuthenticated: false,
-        user: null
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case 'LOGIN':
+			localStorage.setItem('user', JSON.stringify(action.payload.user));
+			localStorage.setItem('token', JSON.stringify(action.payload.token));
+			return {
+				...state,
+				isAuthenticated: true,
+				user: action.payload.user,
+				token: action.payload.token,
+			};
+		case 'LOGOUT':
+			localStorage.clear();
+			return {
+				...state,
+				isAuthenticated: false,
+				user: null,
+			};
+		default:
+			return state;
+	}
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+	const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'))
-    const token = JSON.parse(localStorage.getItem('token'))
+	useEffect(() => {
+		const user = JSON.parse(localStorage.getItem('user'));
+		const token = JSON.parse(localStorage.getItem('token'));
 
-    if(user && token){
-      dispatch({
-        type: 'LOGIN',
-        payload: {
-          user,
-          token
-        }
-      })
-    }
-  }, [])
-  return (
-    <AuthContext.Provider
-      value={{
-        state,
-        dispatch
-      }}
-    >
-      <Router>
-        <Header />
-        <div className="App">{!state.isAuthenticated ? <Login /> : <Home />}</div>
-      </Router>
-    </AuthContext.Provider>
-  );
+		if (user && token) {
+			dispatch({
+				type: 'LOGIN',
+				payload: {
+					user,
+					token,
+				},
+			});
+		}
+	}, []);
+	return (
+		<AuthContext.Provider
+			value={{
+				state,
+				dispatch,
+			}}>
+			<Router>
+				<Header />
+				<div className='App'>
+					{!state.isAuthenticated ? <Login /> : <Home />}
+				</div>
+			</Router>
+		</AuthContext.Provider>
+	);
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,155 +1,153 @@
-import React, { useContext, useState } from "react";
-import { NavLink, Switch, Route } from 'react-router-dom'
-import logo from "../logo.svg";
-import { AuthContext } from "../App";
-import Signup from "./Signup";
+import React, { useContext, useState } from 'react';
+import { NavLink, Switch, Route } from 'react-router-dom';
+import logo from '../logo.svg';
+import { AuthContext } from '../App';
 
 export const Login = () => {
-  const { dispatch } = useContext(AuthContext);
-  const initialState = {
-    email: "",
-    password: "",
-    isSubmitting: false,
-    errorMessage: null
-  };
+	const { dispatch } = useContext(AuthContext);
+	const initialState = {
+		email: '',
+		password: '',
+		isSubmitting: false,
+		errorMessage: null,
+	};
 
-  const [data, setData] = useState(initialState);
+	const [data, setData] = useState(initialState);
 
-  const handleInputChange = event => {
-    // const [name, value] = event.target;
+	const handleInputChange = event => {
+		setData({
+			...data,
+			[event.target.name]: event.target.value,
+		});
+	};
 
-    setData({
-      ...data,
-      [event.target.name]: event.target.value
-    });
-  };
+	const handleFormSubmit = event => {
+		event.preventDefault();
+		setData({
+			...data,
+			isSubmitting: true,
+			errorMessage: null,
+		});
+		fetch('http://localhost:5000/api/v1/auth/sign-in/', {
+			//**Remember to implement .env here */
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email: data.email,
+				password: data.password,
+			}),
+		})
+			.then(res => {
+				if (res.ok) {
+					return res.json();
+				}
+				throw res;
+			})
+			.then(resJson => {
+				dispatch({
+					type: 'LOGIN',
+					payload: resJson,
+				});
+			})
+			.catch(error => {
+				setData({
+					...data,
+					isSubmitting: false,
+					errorMessage: error.message || error.statusText,
+				});
+			});
+	};
 
-  const handleFormSubmit = event => {
-    event.preventDefault();
-    setData({
-      ...data,
-      isSubmitting: true,
-      errorMessage: null
-    });
-    fetch("http://localhost:5000/api/v1/auth/sign-in/", { //**Remember to implement .env here */
-      method: "post",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        email: data.email,
-        password: data.password
-      })
-    })
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw res;
-      })
-      .then(resJson => {
-        dispatch({
-            type: "LOGIN",
-            payload: resJson
-        })
-      })
-      .catch(error => {
-        setData({
-          ...data,
-          isSubmitting: false,
-          errorMessage: error.message || error.statusText
-        });
-      });
-  };
+	return (
+		<div className='login-container'>
+			<div className='card'>
+				<div className='container'>
+					<header>
+						<NavLink to='/login'>Login</NavLink>
+						<NavLink to='/sign-up'>Sign up</NavLink>
+					</header>
+					<Switch>
+						<Route path='/login'>
+							<form onSubmit={handleFormSubmit}>
+								<label htmlFor='email'>
+									Email Address
+									<input
+										type='text'
+										value={data.email}
+										onChange={handleInputChange}
+										name='email'
+										id='email'
+									/>
+								</label>
 
-  return (
-    <div className="login-container">
-      <div className="card">
-        <div className="container">
-            <header>
-                <NavLink to='/login'>Login</NavLink>
-                <NavLink to='/sign-up'>Sign up</NavLink>
-            </header>
-            <Switch>
-                <Route path='/login'>
-                    <form onSubmit={handleFormSubmit}>
-                        <label htmlFor="email">
-                            Email Address
-                            <input
-                                type="text"
-                                value={data.email}
-                                onChange={handleInputChange}
-                                name="email"
-                                id="email"
-                            />
-                        </label>
+								<label htmlFor='password'>
+									Password
+									<input
+										type='password'
+										value={data.password}
+										onChange={handleInputChange}
+										name='password'
+										id='password'
+									/>
+								</label>
 
-                        <label htmlFor="password">
-                            Password
-                            <input
-                                type="password"
-                                value={data.password}
-                                onChange={handleInputChange}
-                                name="password"
-                                id="password"
-                            />
-                        </label>
+								{data.errorMessage && (
+									<span className='form-error'>{data.errorMessage}</span>
+								)}
 
-                        {data.errorMessage && (
-                        <span className="form-error">{data.errorMessage}</span>
-                        )}
+								<button disabled={data.isSubmitting}>
+									{data.isSubmitting ? (
+										<img className='spinner' src={logo} alt='loading icon' />
+									) : (
+										'Login'
+									)}
+								</button>
+							</form>
+						</Route>
+						<Route path='/sign-up'>
+							<form onSubmit={handleFormSubmit}>
+								<label htmlFor='email'>
+									Email Address
+									<input
+										type='text'
+										value={data.email}
+										onChange={handleInputChange}
+										name='email'
+										id='email'
+									/>
+								</label>
 
-                        <button disabled={data.isSubmitting}>
-                            {data.isSubmitting ? (
-                                <img className="spinner" src={logo} alt="loading icon" />
-                            ) : (
-                                "Login"
-                            )}
-                        </button>
-                    </form>
-                </Route>
-                <Route path='/sign-up'>
-                    <form onSubmit={handleFormSubmit}>
-                        <label htmlFor="email">
-                            Email Address
-                            <input
-                                type="text"
-                                value={data.email}
-                                onChange={handleInputChange}
-                                name="email"
-                                id="email"
-                            />
-                        </label>
+								<label htmlFor='password'>
+									Password
+									<input
+										type='password'
+										value={data.password}
+										onChange={handleInputChange}
+										name='password'
+										id='password'
+									/>
+								</label>
 
-                        <label htmlFor="password">
-                        Password
-                        <input
-                            type="password"
-                            value={data.password}
-                            onChange={handleInputChange}
-                            name="password"
-                            id="password"
-                        />
-                        </label>
+								{data.errorMessage && (
+									<span className='form-error'>{data.errorMessage}</span>
+								)}
 
-                        {data.errorMessage && (
-                        <span className="form-error">{data.errorMessage}</span>
-                        )}
-
-                        <button disabled={data.isSubmitting}>
-                            {data.isSubmitting ? (
-                                <img className="spinner" src={logo} alt="loading icon" />
-                            ) : (
-                                "Sign up"
-                            )}
-                        </button>
-                    </form>
-                </Route>
-            </Switch>
-        </div>
-      </div>
-    </div>
-  );
+								<button disabled={data.isSubmitting}>
+									{data.isSubmitting ? (
+										<img className='spinner' src={logo} alt='loading icon' />
+									) : (
+										'Sign up'
+									)}
+								</button>
+							</form>
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
