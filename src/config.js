8ea47d02b0833ecc5e03154d79cd8efc03265f8f
Sam Odum <sam.odum1@gmail.com>
2020-06-03 16:31:53 +0100

    Make config.js public
    Because:
    - It does contain variables caalled in brain.js
    - It does not contain anything I'd consider sensitive
    
    Changed the status of the file from private to public.

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const clarifaiApiKey = 'd16ebb66b3bd4faa929048ce85fb4b7e';
+export const signUpApi = 'http://localhost:5000/api/v1/auth/signup/';
+export const logInApi = 'http://localhost:5000/api/v1/auth/login/';
+export default clarifaiApiKey;
